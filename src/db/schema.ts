55423a74895e06a819d803c0cd6897c469605463
riccardobattiato <riccardobattiato@proton.me>
2024-10-18 18:23:43 +0200

    chore: drizzle-zod install

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,14 @@
 import { integer, pgTable, varchar, text, boolean } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
-export const usersTable = pgTable("tasks", {
+export const tasks = pgTable("tasks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   description: text(),
   completed: boolean().default(false).notNull(),
 });
+
+// Schema for inserting a user - can be used to validate API requests
+const insertTaskSchema = createInsertSchema(tasks);
+// Schema for selecting a user - can be used to validate API responses
+const selectTaskSchema = createSelectSchema(tasks);
